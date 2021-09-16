7bf2cca66815ae0f5a410321da2a261d334f3e72
Jacques Mac <mac@Jacques-MacBook-Pro.local>
2021-09-16 08:27:16 +0100

    fix: preload fontface in _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,9 +14,16 @@ class CustomDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=B612:ital,wght@0,400;0,700;1,400;1,700&family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
           />
-          <link
+          {/* <link
             href="http://fonts.cdnfonts.com/css/circular-std-book"
             rel="stylesheet"
+          /> */}
+          <link
+            rel="preload"
+            href="/fonts/CircularStd-Bold.otf"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
